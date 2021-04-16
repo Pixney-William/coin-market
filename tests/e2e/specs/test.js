b6cf6476654a8f76c08cf551b456448195f88ced
William Åström <william@pixney.com>
2021-04-16 03:18:41 +0200

    End to End tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,17 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
-  it("Visits the app root url", () => {
+  it("Visits the app root url and see a bitcoin card", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("Bitcoin");
+  });
+  it("Clicks the bitcoin card and reaches its url.", () => {
+    cy.visit("/");
+    cy.get("[data-cy=BTC] a").click()
+    cy.url().should('eq', 'http://localhost:8081/BTC')
+  });
+  it("Visits the Etherum page directly.", () => {
+    cy.visit("http://localhost:8081/ETH");
+    cy.contains("Ethereum");
   });
 });
