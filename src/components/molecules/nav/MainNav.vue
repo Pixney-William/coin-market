b8853e9b23f1701a1df44d495d73ec5acf91d212
William Åström <william@pixney.com>
2021-04-16 04:59:14 +0200

    Moved styles to the correct component

--- src/components/molecules/nav/MainNav.vue
+++ src/components/molecules/nav/MainNav.vue
@@ -9,3 +9,12 @@
         </ul>
     </nav>
 </template>
+
+<style >
+.c-nav {
+    @apply my-16 xl:my-24;
+}
+.c-nav ul {
+    @apply flex justify-center text-center text-3xl lg:text-3xl font-extralight tracking-wide;
+}
+</style>
