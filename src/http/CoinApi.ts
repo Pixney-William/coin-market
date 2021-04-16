9cfad6377d83214ba6c35bd3687a95aabe304fc4
William Åström <william@pixney.com>
2021-04-16 11:14:59 +0200

    Added missing types for Axios client

--- src/http/CoinApi.ts
+++ src/http/CoinApi.ts
@@ -1,12 +1,14 @@
-import axios from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const coinApi = axios.create({
+const config: AxiosRequestConfig = {
   baseURL: process.env.VUE_APP_BASE_URL,
   timeout: 15000,
   headers: {
     "Content-Type": "application/json",
     "X-CoinAPI-Key": process.env.VUE_APP_COINAPI_KEY,
   },
-});
+};
+
+const coinApi: AxiosInstance = axios.create(config);
 
 export { coinApi };
