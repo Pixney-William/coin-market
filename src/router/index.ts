f60d3eba709640b2111ac184f3f8d68771975b05
William Åström <william@pixney.com>
2021-04-16 04:51:02 +0200

    Renaming

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,8 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/:id",
     name: "Currency",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Currency.vue"),
+      import(/* webpackChunkName: "currency" */ "../views/Currency.vue"),
   },
 ];
 
