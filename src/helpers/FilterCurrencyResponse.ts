7e320957dfc727d10fd5137809a455bf3d88236d
William Åström <william@pixney.com>
2021-04-16 04:55:24 +0200

    Removed Junk

--- src/helpers/FilterCurrencyResponse.ts
+++
@@ -1,7 +0,0 @@
-// const filterCurrencyResponse = () => {
-//     data.filter((curr) => {
-//         //     if (curr.type_is_crypto === 1 && curr.price_usd)
-//         //         return curr;
-//         // });
-// }
-// export { filterCurrencyResponse }
